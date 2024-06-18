edbaa0a73b4f98812a83da248f585e281f0f5ea0
lenonleite <lenonleite@gmail.com>
2024-06-18 10:53:22 +0100

    - add companytagevents - add dispatch on update and create companies

---
+++ Event/CompanyTagsEvent.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace MauticPlugin\LeuchtfeuerCompanyTagsBundle\Event;
+
+use Mautic\CoreBundle\Event\CommonEvent;
+use Mautic\LeadBundle\Entity\Company;
+use MauticPlugin\LeuchtfeuerCompanyTagsBundle\Entity\CompanyTags;
+
+class CompanyTagsEvent extends CommonEvent
+{
+    private Company $company;
+
+    public function __construct(Company $company)
+    {
+        $this->company = $company;
+    }
+
+    public function getCompany(): Company
+    {
+        return $this->company;
+    }
+
+
+}
