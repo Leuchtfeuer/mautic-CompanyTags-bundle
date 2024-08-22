9cddab7b46c3beec92758ea99987928d55cf66ba
Ekkehard Gümbel <43146234+ekkeguembel@users.noreply.github.com>
2024-08-22 17:46:14 +0200

    bump release number

--- Config/config.php
+++ Config/config.php
@@ -3,7 +3,7 @@
 return [
     'name'        => 'Company Tags by Leuchtfeuer',
     'description' => 'Provide a 2nd type of Tags which can be applied to Companies',
-    'version'     => '1.0.0',
+    'version'     => '1.1.1',
     'author'      => 'Leuchtfeuer Digital Marketing GmbH',
     'services'    => [
         'integrations' => [
