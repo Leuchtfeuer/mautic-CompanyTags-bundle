07c16357184849546624318d457d8eb66fee409b
Jonas <j.ludwig@leuchtfeuer.com>
2026-01-08 13:57:37 +0100

    MTC 9219 Enable renaming tags

--- Form/Type/CompanyTagEntityType.php
+++ Form/Type/CompanyTagEntityType.php
@@ -20,8 +20,6 @@ class CompanyTagEntityType extends AbstractType
     {
         $builder->add('buttons', FormButtonsType::class);
         $builder->addEventSubscriber(new CleanFormSubscriber(['description' => 'strict_html']));
-        // We only allow to set tag field value if we are creating new tag.
-        $tagReadOnly = !empty($options['data']) && $options['data']->getId() ? true : false;
 
         $builder->add(
             'tag',
@@ -29,7 +27,7 @@ class CompanyTagEntityType extends AbstractType
             [
                 'label'       => 'mautic.core.name',
                 'label_attr'  => ['class' => 'control-label'],
-                'attr'        => ['class' => 'form-control', 'readonly' => $tagReadOnly],
+                'attr'        => ['class' => 'form-control'],
                 'constraints' => [
                     new NotBlank(
                         [
